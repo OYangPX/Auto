a9fd0e08149d0971e3201dbc9238a56aca971702
lonel <p_pengjiaxuan@xinfintech.com>
2019-03-28 18:33:48 +0800

    '添加bootstrap'

--- app/views.py
+++ app/views.py
@@ -2,21 +2,29 @@
 from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
+from .models import User
+from flask_login import LoginManager  
+from flask_login import login_user, login_required  
 
-@app.route('/')
 @app.route('/index')
 def index():
     user ={'lonel':'ioan'}
     return render_template('index.html',title='home',user=user)
     #return 'flask Web framework'
 
-
+@app.route('/')
 @app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('index')
-    return render_template('login.html',title='Sign In' ,form=form
-    #,providers = app.config['OPENID_PROVIDERS']
-    )
+    if request.method=='POST':
+        if not form.validate_on_submit():
+            print(form.errors)
+            return render_template('login.html',form=form)
+
+        user = User.query.filter(User.name == form.name.data,User.password == form.password.data).first()
+        if user:
+            login_user(user)
+            return render_template('index.html')
+
+    return  render_template('login.html',form=form)
+    
